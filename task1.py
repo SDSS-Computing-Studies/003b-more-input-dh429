f068e14fb3fcf5c3eb2e20ce0274b4b2dd689bb7
dh429 <90715528+dh429@users.noreply.github.com>
2021-09-27 10:20:53 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -16,7 +16,7 @@ You earned $0.20 interest.
 """
 import math
 
-principle = input("Enter your amount:").strip()
+principle = input("Enter your amount:")
 principle = float(principle)
 
 rate = input("Enter the rate:").strip()
